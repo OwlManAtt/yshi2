855bfd0f8d67b64469ca0345a91044f982f67e8a
Nicholas 'OwlManAtt' Evans <owlmanatt@gmail.com>
2012-06-20 21:31:58 -0400

    Initial commit of the Rails skeleton.
    A land of magic awaits.

--- README.md
+++
@@ -1,4 +0,0 @@
-yshi2
-=====
-
-An open-sores EVE industrial toolkit that might not suck.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yshi2::Application.config.secret_token = 'cc7329b0c3660d8882f0241b7453b9a3291146013b5bbaae1fb4f6c233c9dd5dd0f7cbd9d6df8da185eb51d167a400df5090a575b50e05c15228b25eccca4b84'
